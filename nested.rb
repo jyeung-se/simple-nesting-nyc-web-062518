576406867561bbb837461d0c7ddcbcf604faa5e4
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 23:47:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,11 +87,12 @@ def adding_matz
         }
      }
 
-  programmer_hash[] = {}
-  # programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  # programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
   
-    
+  programmer_hash
+  
 end
 
 def changing_alan
